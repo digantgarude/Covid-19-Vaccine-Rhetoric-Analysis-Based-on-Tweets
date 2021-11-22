d21e226e6213d74447d74152882ac3a1e44cd237
digantgarude <digant.dg07@gmail.com>
2021-11-22 14:02:02 -0500

    Added base code for sentiment analysis

---
+++ cred.py
@@ -0,0 +1,9 @@
+user_name = "project4"
+PASSWORD = ""
+aws_access_key_id = "AKIASRBDDSUHTFPVZEW7"
+aws_secret_access_key = "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"
+CONSOLE_LOGIN_LINK = "https://174019810575.signin.aws.amazon.com/console"
+region="us-east-1"
+
+CORE_NAME = "IRF21P1"
+AWS_IP = "18.216.184.141"
