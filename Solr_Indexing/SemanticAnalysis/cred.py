ce84b19f77473ed771004a94aefda40e1bb47806
digantgarude <digant.dg07@gmail.com>
2021-11-28 18:47:28 -0500

    Updated

--- Solr_Indexing/SemanticAnalysis/cred.py
+++ Solr_Indexing/SemanticAnalysis/cred.py
@@ -1,7 +1,5 @@
 user_name = "project4"
 PASSWORD = ""
-aws_access_key_id = "AKIASRBDDSUHTFPVZEW7"
-aws_secret_access_key = "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"
 CONSOLE_LOGIN_LINK = "https://174019810575.signin.aws.amazon.com/console"
 region="us-east-1"
 
