47c6fe3a1d86a6eab0829dbaa812c654b6ff4abd
digantgarude <digant.dg07@gmail.com>
2021-11-28 18:44:27 -0500

    Fixed bugs

--- Solr_Indexing/SemanticAnalysis/run_sentiment_analysis.py
+++ Solr_Indexing/SemanticAnalysis/run_sentiment_analysis.py
@@ -1,16 +1,16 @@
-from Solr_Indexing.SemanticAnalysis.indexer import AWS_IP
 import sentiment_analysis
 
 
 sa = sentiment_analysis.SentimentAnalysis()
 
 CORE_NAME_FETCH = "IRF21P1"
-CORE_NAME_UPLOAD = "IR_P4"
+CORE_NAME_UPLOAD = "BF_Project4"
 AWS_IP_FETCH = "3.141.19.45"
-AWS_IP_UPLOAD = "3.141.19.45"
+AWS_IP_UPLOAD = "3.21.230.103"
 
-sa.connection(CORE_NAME_FETCH, AWS_IP)
+sa.connect(CORE_NAME_FETCH, AWS_IP_FETCH)
+# 100000
 sa.get_tweets(no_of_rows=100000)
 sa.perform_analysis()
-sa.connection(CORE_NAME_UPLOAD, AWS_IP_UPLOAD)
+sa.connect(CORE_NAME_UPLOAD, AWS_IP_UPLOAD)
 sa.upload_modified_tweets()
