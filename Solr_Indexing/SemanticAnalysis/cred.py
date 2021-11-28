9a2609f228d9c23fec915b5534fea86c060f9c13
digantgarude <digant.dg07@gmail.com>
2021-11-28 17:03:05 -0500

    Added sentiment analysis class

--- Solr_Indexing/SemanticAnalysis/cred.py
+++ Solr_Indexing/SemanticAnalysis/cred.py
@@ -5,5 +5,12 @@ aws_secret_access_key = "ibof268xB7RWGs6PyA/zXVuxkBsufJz4IrjQ07/w"
 CONSOLE_LOGIN_LINK = "https://174019810575.signin.aws.amazon.com/console"
 region="us-east-1"
 
-CORE_NAME = "IRF21P1"
-AWS_IP = "3.141.19.45"
+CORE_NAME_FETCH = "IRF21P1"
+CORE_NAME_UPLOAD = "IR_P4"
+
+# AWS_IP_HG = "3.141.19.45"
+# AWS_IP_DG = "18.216.184.141"
+
+# Using DG
+# AWS_IP = "18.216.184.141"
+# AWS_IP = "3.141.19.45"
