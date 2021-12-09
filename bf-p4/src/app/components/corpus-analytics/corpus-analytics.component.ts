6a1a7039ddf4bc54faeb8cceb8cb6590754760b6
digantgarude <digant.dg07@gmail.com>
2021-12-09 01:32:24 -0500

    Updated

--- bf-p4/src/app/components/corpus-analytics/corpus-analytics.component.ts
+++ bf-p4/src/app/components/corpus-analytics/corpus-analytics.component.ts
@@ -1,15 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { SpinnerService } from './../../services/spinner.service';
 @Component({
   selector: 'app-corpus-analytics',
   templateUrl: './corpus-analytics.component.html',
   styleUrls: ['./corpus-analytics.component.scss']
 })
 export class CorpusAnalyticsComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
